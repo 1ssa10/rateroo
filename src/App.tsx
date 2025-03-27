30b20dd5d2990c44dc40db938ed6600064f2aecc
1ssa10 <mahmoudisaa10@gmail.com>
2025-03-27 22:19:53 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            without the Hassle
+          </h1>
+        </header>
+      </div>
+    </main>
+  );
+}
+
+export default App;
